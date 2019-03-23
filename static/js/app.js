86e021187991cab0860f563e65ef4c0ccdc808eb
Matt <matt@mwjohnston.com>
2019-03-22 19:59:20 -0700

    fixed pie chart.  styling still a bit off, bgcolor isnt working

--- static/js/app.js
+++ static/js/app.js
@@ -11,6 +11,29 @@ function leaderboard() {
     });
 }
 
+function character(datas) {
+    console.log('Character function');
+     d3.select("#test2")
+       .selectAll(".bio")
+       .data(datas)
+       .enter()
+       .append(`div`)
+       .html(data => { return `<h4 class="ranking"><strong>Rank ${data.ranking}</strong></h4><h4 class="rating" style="margin-left:10px"><strong> Rating ${data.rating}</strong></h4><br>
+             <div class="row">
+              <img src="/static/Resources/Class/${data.classId}.jpg" width="30" height="30"></img>
+              <img src="/static/Resources/specialization/${data.classId}/${data.specId}.jpg" width="30" height="30"></img>
+              <a href="https://worldofwarcraft.com/en-us/character/us/${data.realmSlug}/${data.name}" style="text-decoration: underline;color:black" target="_blank" rel="nofollow">${data.name} </a>
+              <p style="margin-left:10px">${data.tier} Tier</><img src="/static/Resources/tier/${data.tier}.png" width="30" height="30"></img>
+              </div>
+              <div class="row">
+              <img src="/static/Resources/faction/${data.factionId}.jpg" width="30" height="30"></img>
+              <p>Wins:${data.seasonWins} Played:${data.seasonWins + data.seasonLosses}</p>
+              </div>`})
+       .attr("class", data => {return `${data.classId}`})
+       .classed("col-md-4", true);
+ }
+
+
 function piechart() {
     const keys = Array();
     const values = Array();
@@ -20,30 +43,22 @@ function piechart() {
             keys.push(key);
             values.push(val);
         });
-    
+        data1 = [{
+            values: values.valueOf(),
+            labels: keys.valueOf(),
+            type: "pie"
+        }];
+        layout1 = {
+            title: "Character Class Breakdown",
+            autosize:true,
+            visible:true,
+            showlegend:true,
+            bgcolor:"rgb(41, 41, 41)",
+            responsive: true
+        };
+        Plotly.newPlot("pie", data1, layout1);
     });
-
-  //  [1:]
-    var layout = {
-        title: "Character Class Breakdown",
-        autosize:true,
-        paper_bgcolor:'#7f7f7f',
-        plot_bgcolor: '#c7c7c7',
-        margin: {
-            l: 50,
-            r: 20,
-            b: 50,
-            t: 50,
-            pad: 10
-          }
-    };
-    var data = [{
-        values: values.valueOf(),
-        labels: keys.valueOf(),
-        type: "pie",
-        visible: true
-    }]; 
-    Plotly.newPlot("pie", data, layout, {responsive: true});
+    
 }
   
 function updatePlotly(newdata) {
@@ -55,7 +70,6 @@ function changeData(classfilter) {
     dataset = $.getJSON('/filter', {classfilter:classfilter}, (data) => {return(data)});
     updatePlotly(dataset);
 }
-  
 
 $('#classfilter').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
     $.getJSON('/filter', {classfilter:$(this).val()}, function(data) {
